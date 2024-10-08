a326ff410dcb61096844ec669e664adca80d2e4b
Judah Paulo <141393097+jowdaaaa@users.noreply.github.com>
2024-10-08 15:54:02 +0800

    Add files via upload

---
+++ BSIT-3F(AY 2024-2025)/INTEGRATIVE PROGRAMMING & TECHNOLOGIES 2/vinasActivity6/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Grade mid = new Midterm();
+        mid.compute();
+        Grade tent = new Midterm();
+        tent.compute();
+    }
+}
